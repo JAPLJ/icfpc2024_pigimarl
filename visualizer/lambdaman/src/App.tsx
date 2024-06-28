6a7c0e923061ea3ef168d59c75b357f7022bab9d
frn.fnts <frn.fnts@gmail.com>
2024-06-29 01:44:16 +0900

    localStorage

--- visualizer/lambdaman/src/App.tsx
+++ visualizer/lambdaman/src/App.tsx
@@ -1,14 +1,36 @@
 import './App.css'
 import { LambdaManEmulator } from './LambdaManEmulator'
 import { CellType } from 'src/types';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react'
 
 function App() {
   const [field, setField] = useState<CellType[][]>([])
+  const fieldRef = useRef<HTMLTextAreaElement>(null)
+
+  useEffect(() => {
+    if (field.length === 0) {
+      return;
+    }
+    localStorage.setItem("field", JSON.stringify(field));
+  }, [field]);
+
+  useEffect(() => {
+    const storedField = localStorage.getItem("field");
+    if (storedField) {
+      const parsedField = JSON.parse(storedField);
+      setField(parsedField);
+      if (fieldRef.current) {
+        fieldRef.current.value = parsedField.map((line: CellType[]) => line.join("")).join("\n");
+      }
+    }
+  }, []);
+
+
   return (
     <>
       <h1>Input field</h1>
       <textarea
+        ref={fieldRef}
         className="filedInput"
         onChange={(e) => {
           const f = e.target.value
